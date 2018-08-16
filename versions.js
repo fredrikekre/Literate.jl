cba2919732ed98e09960ded40f54a226b74f1b4c
autodocs <autodocs>
2018-08-16 20:46:55 +0000

    build based on 32a6531

--- versions.js
+++ versions.js
@@ -1,14 +1,10 @@
 var DOC_VERSIONS = [
   "stable",
-  "latest",
-  "release-1.0",
-  "release-0.3",
-  "release-0.2",
-  "release-0.1",
   "v1.0.0",
   "v0.3.0",
   "v0.2.2",
   "v0.2.1",
   "v0.2.0",
   "v0.1.0",
+  "dev",
 ];
