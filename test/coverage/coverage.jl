f0adce8593d6a5cff0845a4ecf4473cb2ef71503
Fredrik Ekre <fredrik.ekre@chalmers.se>
2018-08-24 09:21:15 +0200

    Actually deploy coverage.

--- test/coverage/coverage.jl
+++ test/coverage/coverage.jl
@@ -1,3 +1,7 @@
+# only push coverage from one bot
+get(ENV, "TRAVIS_OS_NAME", nothing)       == "linux" || exit(0)
+get(ENV, "TRAVIS_JULIA_VERSION", nothing) == "1.0"   || exit(0)
+
 using Coverage
 
 cd(joinpath(@__DIR__, "..", "..")) do
