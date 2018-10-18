8241309d33b76b39fce24aa0748a0f888394d518
Fredrik Ekre <fredrik.ekre@chalmers.se>
2018-10-19 00:33:45 +0200

    Simplify code coverage submission.

--- test/coverage/coverage.jl
+++ test/coverage/coverage.jl
@@ -3,7 +3,4 @@ get(ENV, "TRAVIS_OS_NAME", nothing)       == "linux" || exit(0)
 get(ENV, "TRAVIS_JULIA_VERSION", nothing) == "1.0"   || exit(0)
 
 using Coverage
-
-cd(joinpath(@__DIR__, "..", "..")) do
-    Codecov.submit(Codecov.process_folder())
-end
+Codecov.submit(Codecov.process_folder())
