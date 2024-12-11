12871fa83c4df23fa74991f9f6cbf1c1f0cfc49c
Documenter.jl <documenter@juliadocs.github.io>
2024-12-11 13:54:29 +0000

    build based on 5d1b491

--- dev/generated/example.jl
+++ dev/generated/example.jl
@@ -1,5 +1,5 @@
-x = 1//3
-y = 2//5
+x = 1 // 3
+y = 2 // 5
 
 x + y
 
@@ -15,7 +15,7 @@ foo()
 1 + 1;
 
 using Plots
-x = range(0, stop=6π, length=1000)
+x = range(0, stop = 6π, length = 1000)
 y1 = sin.(x)
 y2 = cos.(x)
 plot(x, [y1, y2])
